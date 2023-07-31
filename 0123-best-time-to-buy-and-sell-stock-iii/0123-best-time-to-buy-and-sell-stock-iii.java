3fed18d83693544204e5f10710e10b8abcda2972
shlok008 <55846431+shlok008@users.noreply.github.com>
2023-07-31 20:24:27 +0530

    Time: 61 ms (45.97%), Space: 62.7 MB (35.94%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -17,8 +17,29 @@ class Solution {
         return dp[ind][cart][tran]=Math.max(take,nottake);
     }
     public int maxProfit(int[] p) {
-        int dp[][][]=new int[p.length][2][3];
-        for(int i[][]:dp) for(int[] j:i)Arrays.fill(j,-1);
-        return rec(0,p,0,dp,2);
+        int dp[][][]=new int[p.length+1][2][3];
+        //for(int i[][]:dp) for(int[] j:i)Arrays.fill(j,-1);
+        for(int ind=p.length-1;ind>=0;--ind)
+        {
+            for(int cart=1;cart>=0;--cart)
+            {
+                for(int tran=1;tran<=2;++tran)
+                {
+                    int take,nottake;
+                    if(cart==0)
+                    {
+                        take =-p[ind] +dp[ind+1][1][tran];
+                        nottake=dp[ind+1][0][tran];
+                    }
+                    else
+                    {
+                        take=p[ind]+dp[ind+1][0][tran-1];
+                        nottake=dp[ind+1][1][tran];
+                    }
+                    dp[ind][cart][tran]=Math.max(take,nottake);
+                }
+            }
+        }
+        return dp[0][0][2];
     }
 }
