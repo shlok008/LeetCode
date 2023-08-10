b1f0dbc031373c4e870a2b113ed6ab08c93a1d5f
shlok008 <55846431+shlok008@users.noreply.github.com>
2023-08-10 21:09:50 +0530

    Time: 28 ms (54.83%), Space: 44.1 MB (76.72%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -2,7 +2,7 @@ class Solution {
     public int minEatingSpeed(int[] p, int h) {
         int end=p[0];
         for(int i=1;i<p.length;++i) if(end<p[i])end=p[i];
-        int start=0;
+        int start=1;
         int a=0;
         while(start<=end)
         {
@@ -10,26 +10,8 @@ class Solution {
             int ans=0;
             for(int i=0;i<p.length;++i) ans+=Math.ceil(p[i]*1.0/mid);
             if(ans<=h) {a=mid;end=mid-1;}
-            else start=mid+1; 
+            else {start=mid+1;}
         }
         return a;
     }
 }
-/*
-class Solution {
-    public int minEatingSpeed(int[] p, int h) {
-        int end=p[0];
-        for(int i=1;i<p.length;++i) if(end<p[i])end=p[i];
-        int start=0;
-        int a=0;
-        while(start<=end)
-        {
-            int mid=(start+end)/2;
-            int ans=0;
-            for(int i=0;i<p.length;++i) ans+=Math.ceil(p[i]*1.0/mid);
-            if(ans<=h) {a=mid;end=mid-1;}
-            else start=mid+1; 
-        }
-        return a;
-    }
-}*/
