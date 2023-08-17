3d72306d4b79fb26c97a5053e98200a8f32a8088
shlok008 <55846431+shlok008@users.noreply.github.com>
2023-08-17 18:03:05 +0530

    Time: 0 ms (100%), Space: 41.3 MB (20.71%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,6 +1,20 @@
 class Solution {
     public boolean searchMatrix(int[][] mat, int tar) {
-        int m=0,n=0;
+        int start=0;
+        int end=mat.length*mat[0].length;end--;
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            int m=mid/mat[0].length;
+            int n=mid-m*mat[0].length;
+            if(tar==mat[m][n]) return true;
+            else if(tar>mat[m][n])start=mid+1;
+            else end=mid-1;
+        }
+        return false;
+    }
+}
+        /*int m=0;
         while(m<mat.length)
         {
             if(tar==mat[m][mat[0].length-1])
@@ -10,14 +24,13 @@ class Solution {
             else break;
         }
         if(m==mat.length) return false;
-        while(n<mat[0].length)
+        int start=0;int end=mat[0].length;
+        while(start<=end)
         {
-            if(tar==mat[m][n])
+            int mid=(start+end)/2;
+            if(tar==mat[m][mid])
             return true;
-            else if(tar>mat[m][n])
-            n++;
-            else break;
+            else if(tar>mat[m][mid])start=mid+1;
+            else end=mid-1;
         }
-        return false;
-    }
-}
+        return false;*/
