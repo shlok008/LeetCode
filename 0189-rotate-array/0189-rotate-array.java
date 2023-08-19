d91ff119cf918f787d54decbb3591359c505822b
shlok008 <55846431+shlok008@users.noreply.github.com>
2023-08-19 11:47:09 +0530

    Time: 0 ms (100%), Space: 55.5 MB (71.05%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,5 +1,27 @@
 class Solution {
+    void rev(int[] arr,int start, int end)
+    {
+        while(start<end)
+        {
+            arr[start]=arr[start]+arr[end];
+            arr[end]=arr[start]-arr[end];
+            arr[start]=arr[start]-arr[end];
+            start++;
+            end--;
+        }
+    }
     public int[] rotate(int[] no, int k) {
+        k%=no.length;
+        //rev(no,0,no.length-1);
+        //rev(no,0,k-1);
+        //rev(no,k,no.length-1);
+        rev(no,0,no.length-k-1);
+        rev(no,no.length-k,no.length-1);
+        rev(no,0,no.length-1);
+        return no;
+    }
+}
+/*
         k%=no.length;
         if(k==0) return no;
         int arr[]=new int[k];
@@ -12,5 +34,4 @@ class Solution {
         for(int i=0;i<k;++i)
             no[i]=arr[i];
         return no;
-    }
-}
+*/
