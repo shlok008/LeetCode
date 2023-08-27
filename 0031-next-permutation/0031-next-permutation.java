6ea142c35613a89d9029f9ff0eb64fa544726da2
shlok008 <55846431+shlok008@users.noreply.github.com>
2023-08-27 16:25:30 +0530

    Time: 1 ms (26.22%), Space: 42.4 MB (17.2%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] nextPermutation(int[] n) {
+        int a=n.length-1;
+        while(a>0 && n[a-1]>=n[a])a--;
+        if(a!=0)
+        {
+            int start=a;
+            int end=n.length-1;
+            while(start<=end)
+            {
+                int mid=(start+end)/2;
+                if(n[mid]>n[a-1]) start=mid+1;
+                else end=mid-1;
+            }
+            n[a-1]+=n[end];
+            n[end]=n[a-1]-n[end];
+            n[a-1]=n[a-1]-n[end];
+        }
+        for(int i=a;i<n.length;++i)
+        {
+            int t=i;
+            for(int j=i+1;j<n.length;++j)
+            {if(n[j]<n[t])t=j;}
+            int b=n[i];
+            n[i]=n[t];
+            n[t]=b;
+        }
+        return n;
+    }
+}
