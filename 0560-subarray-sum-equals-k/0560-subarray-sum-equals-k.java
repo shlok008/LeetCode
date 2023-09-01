6a3046447343f448b6c827cc828fd19272b7dc76
shlok008 <55846431+shlok008@users.noreply.github.com>
2023-09-01 16:31:17 +0530

    Time: 22 ms (93.18%), Space: 46.7 MB (22.08%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,17 +1,24 @@
 class Solution {
+    /*void rec(int sum,int ind,List<Integer> l,int[]n, int k)
+    {
+        if(k==sum){System.out.print(l);return;}
+        if(ind==n.length)return;
+        l.add(n[ind]);
+        rec(sum+n[ind],ind+1,l,n,k);
+        l.remove(l.size()-1);
+        rec(sum,ind+1,l,n,k);
+    }*/
     public int subarraySum(int[] n, int k) {
-        int c=0;
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        int s=0;
+        int ans=0;
+        hm.put(0,1);
         for(int i=0;i<n.length;++i)
         {
-            int j=i;
-            int s=0;
-            while(j<n.length)
-            {
-                s+=n[j];
-                if(s==k) c++;
-                j++;
-            }
+            s+=n[i];
+            if(hm.containsKey(s-k)) ans+=hm.get(s-k);
+            hm.put(s,hm.getOrDefault(s,0)+1);
         }
-        return c;
+        return ans;
     }
 }
