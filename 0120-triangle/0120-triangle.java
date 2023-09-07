b1dbcbd4c74c2a2f17154206479a87d35d2e6336
shlok008 <55846431+shlok008@users.noreply.github.com>
2023-09-07 12:16:55 +0530

    Time: 6 ms (26.95%), Space: 44 MB (60.53%) - LeetHub

---
+++ 0120-triangle/0120-triangle.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> tri) {
+        for(int i=tri.size()-2;i>=0;--i)
+            for(int j=tri.get(i).size()-1;j>=0;--j)
+                tri.get(i).set(j,tri.get(i).get(j)+Math.min(tri.get(i+1).get(j),tri.get(i+1).get(j+1)));
+            return tri.get(0).get(0);
+    }
+}
