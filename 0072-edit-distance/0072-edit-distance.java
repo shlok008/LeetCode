08ffbefda24613cc17e5ca756486bada35614b69
shlok008 <55846431+shlok008@users.noreply.github.com>
2023-09-10 16:48:14 +0530

    Time: 4 ms (82.46%), Space: 43.4 MB (79.72%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -5,9 +5,7 @@ class Solution {
         if(j==0)return i;
         if(m.charAt(i-1)==n.charAt(j-1)) return rec(i-1,j-1,m,n,dp);
         if(dp[i][j]!=-1)return dp[i][j];
-        int del=1+rec(i-1,j,m,n,dp);
-        int rep=1+rec(i-1,j-1,m,n,dp);
-        return dp[i][j]=Math.min(1+rec(i,j-1,m,n,dp),Math.min(del,rep)); 
+        return dp[i][j]=1+Math.min(rec(i,j-1,m,n,dp),Math.min(rec(i-1,j,m,n,dp),rec(i-1,j-1,m,n,dp))); 
     }
     public int minDistance(String m, String n) {
         if(n.length()==0)return m.length();
